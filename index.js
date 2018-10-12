f5a850dd4c5781822d5094eb63df8ed10166c02b
Vika92 <viktoria2401gritsenko@gmail.com>
2018-10-12 08:17:06 +0300

    setup oauth

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ import session from 'express-session';
 const app = express();
 
 app.use(corsHandler);
+app.use(bodyParser.json());
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}));
 
 // routes
 import UserController from './models/user-controller';
+import bodyParser from "body-parser";
 app.use('/auth', UserController);
+app.get('', (req, res) => {
+  res.send('hello!');
+})
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, function () {
   console.log(`Server is running on port ${process.env.APP_PORT}`);
--- models/user-controller.js
+++ models/user-controller.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import axios from 'axios';
 
 import User from './user';
 
@@ -32,7 +33,7 @@ router.get('/logout', function(req, res) {
 });
 
 // POST
-router.post('/register', bodyParser.json(), function(req, res) {
+router.post('/register', function(req, res) {
   const hashedPassword = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
   User.create({
     name: req.body.name,
@@ -44,6 +45,71 @@ router.post('/register', bodyParser.json(), function(req, res) {
       const token = jwt.sign({id: user._id}, 'secret', {expiresIn: 86400});
       req.session.token = token;
       res.status(200).send({auth: true, token: token});
+    });
+});
+
+router.post('/github/access_token', function(req, res) {
+  console.log(52);
+  axios({
+    // params: {
+    //   client_id: process.env.GITHUB_CLIENT_ID,
+    //   client_secret: process.env.GITHUB_CLIENT_SECRET,
+    //   code: req.body.code,
+    //   redirect_uri: process.env.LINKEDIN_REDIRECT_URI
+    // },
+    method: 'post',
+    url: `https://github.com/login/oauth/access_token?client_id=
+    ${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&code=${req.body.code}`,
+    headers: {
+      accept: 'application/json'
+    }
+  }).then((response) => {
+    const accessToken = response.data.access_token;
+    console.log(62, accessToken);
+    // res.status(200).send();
+    // axios.get('https://api.github.com/user', {
+    //   headers: {
+    //     Authorization: 'token ' + accessToken
+    //   }
+    // }).then((response) => {
+    //   console.log(76, response.data);
+    // }).catch(err => {
+    //   console.log(69, err.response.data);
+    // })
+    // res.redirect(`/welcome.html?access_token=${accessToken}`)
+  }).catch(err => {
+
+    console.log(81, err.response);
+  })
+});
+
+router.post('/linkedin/access_token', function(req, res) {
+  console.log(85);
+  // axios.get(`https://www.linkedin.com/oauth/v2/accessToken?client_id=${process.env.LINKEDIN_CLIENT_ID}&grant_type=authorization_code&code=${req.body.code}&client_secret=${process.env.LINKEDIN_CLIENT_SECRET}&redirect_uri=${process.env.LINKEDIN_REDIRECT_URI}`)
+  axios.get('https://www.linkedin.com/oauth/v2/accessToken', {
+    params: {
+      grant_type: 'authorization_code',
+      client_id: process.env.LINKEDIN_CLIENT_ID,
+      client_secret: process.env.LINKEDIN_CLIENT_SECRET,
+      code: req.body.code,
+      redirect_uri: process.env.LINKEDIN_REDIRECT_URI
+    }
+  }).then((response) => {
+      const accessToken = response.data.access_token;
+      console.log(99, accessToken);
+      axios.get('https://api.linkedin.com/v1/people/~:(first-name,email-address)?format=json', {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        }
+      }).then(response => {
+        console.log(105, response.data)
+      }).catch((err) => {
+        console.log(107);
+      })
+      // res.status(200).send();
+    })
+    .catch((error) => {
+      console.log(101);
     });
 });
 
